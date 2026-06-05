5b2780f69c50a349c8d0496351df832afebe1c03
Surya <balasurya2932003@gmail.com>
2026-06-05 12:17:47 +0530

    Add booking creation and update tests

--- Rest_Assured/src/test/java/tests/UpdateTest.java
+++ Rest_Assured/src/test/java/tests/UpdateTest.java
@@ -1,31 +1,59 @@
-package tests;
+    package tests;
 
-import static org.hamcrest.Matchers.equalTo;
-import org.testng.annotations.Test;
+    import static org.hamcrest.Matchers.equalTo;
+    import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+    import io.restassured.RestAssured;
+    import io.restassured.response.Response;
 
-public class UpdateTest extends base.BaseTest{
-    @Test
-public void updateBookingPATCH(){
+    public class UpdateTest extends base.BaseTest{
+        private int bookingId = 0;
+        @Test
+        public void createBooking(){
+            Response r = RestAssured.given()
+            .contentType("application/json")
+            .body("{\n" +
+                    "  \"firstname\": \"Steve\",\n" +
+                    "  \"lastname\": \"Smith\",\n" +
+                    "  \"totalprice\": 1000,\n" +
+                    "  \"depositpaid\": true,\n" +
+                    "  \"bookingdates\": {\n" +
+                    "    \"checkin\": \"2024-01-01\",\n" +
+                    "    \"checkout\": \"2024-01-05\"\n" +
+                    "  }\n" +
+                    "}")
+            .when()
+                .post("/booking");
+            r.then()
+                .statusCode(200);
 
-    RestAssured.given()
-        .header("Authorization", "Bearer " + token)
-        .contentType("application/json")
-        .body("{ \"firstname\": \"Surya\" }")
+            bookingId = r.jsonPath().getInt("bookingid");
+            System.out.println(bookingId);
+            
+            System.out.println(r.asPrettyString());
+        }
 
-    .when()
-        .patch("/booking/1")
+    @Test(dependsOnMethods = "createBooking")
+    public void updateBookingPATCH(){
 
-    .then()
-        .statusCode(200)
-        .body("firstname", equalTo("Surya"));
+        RestAssured.given()
+            .cookie("token",token)
+            .contentType("application/json")
+            .body("{ \"firstname\": \"Surya\" }")
 
-    RestAssured.given()
-    .when()
-        .get("/booking/1")
-    .then()
-    .statusCode(200)
-    .body("firstname", equalTo("Surya"));
+        .when()
+            .patch("/booking/" + bookingId)
+
+        .then()
+            .statusCode(200)
+            .body("firstname", equalTo("Surya"));
+
+            
+        Response r = RestAssured.given()
+        .when()
+            .get("/booking/" + bookingId);
+        r.then()
+        .statusCode(200);
+        System.out.println(r.jsonPath().getString("firstname"));
+        }
     }
-}
